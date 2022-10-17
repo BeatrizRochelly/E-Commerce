ad152571de3293b0ccfc7308fce874c8d359b2bb
Beatrizrochelly10@gmail.com <beatrizrochelly10@gmail.com>
2022-10-17 12:07:44 -0300

    Criação da Model, Repository, Controller e Service de endereco e cliente

---
+++ teste/src/main/java/com/teste/teste/controller/EnderecoController.java
@@ -0,0 +1,27 @@
+package com.teste.teste.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.teste.service.EnderecoService;
+
+@RestController
+@RequestMapping("/endereco")
+@CrossOrigin("*")
+public class EnderecoController {
+
+	@Autowired
+	private EnderecoService enderecoService;
+	
+	@GetMapping
+	public String buscarCep () throws Exception {
+		return this.enderecoService.consultarCep();
+		
+	}
+	
+
+	
+}
